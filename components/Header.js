eaf8ca2014975e0e9fbe0a12ea7519874f3c8e39
smazydev <alizafar1437@gmail.com>
2021-06-14 04:31:06 -0400

    added conditional rendering for header

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,23 @@
 import Link from "next/link";
 
 export default ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: "Sign up", href: "/auth/signup" },
+    !currentUser && { label: "Sign in", href: "/auth/signin" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return <li key={href}>{label}</li>;
+    });
+
   return (
     <nav className="navbar navbar-light bg-light">
       <Link href="/">
         <a className="navbar-brand">Tik-it</a>
       </Link>
       <div className="d-flex justify-content-end">
-        <ul className="nav d-flex align-items-center">
-          {currentUser ? "Sign Out" : "Signin/up"}
-        </ul>
+        <ul className="nav d-flex align-items-center">{link}</ul>
       </div>
     </nav>
   );
