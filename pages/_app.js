1e42b696c433228d2291b7e9a15cefc096f98a25
smazydev <alizafar1437@gmail.com>
2021-06-14 04:05:14 -0400

    added multiple get initialprops

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
 import "bootstrap/dist/css/bootstrap.css";
 
-export default ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps }) => {
   return (
     <div>
+      <h1>Header</h1>
       <Component {...pageProps} />
     </div>
   );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+ 
+  const client = buildClient(appContext.ctx);
+  const data = await client.get('/api/users/currentuser');
+ 
+  return data;
+};
+
+
+export default AppComponent;
