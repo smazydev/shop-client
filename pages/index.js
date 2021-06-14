5e84e39b8481e5882d9a7ea5ae430bbb04281950
smazydev <alizafar1437@gmail.com>
2021-06-14 03:46:59 -0400

    did stuff

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,15 @@
-import axios from "axios";
-
+import buildClient from "../api/build-client";
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>you are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-	      headers: req.headers
-      }
-    );
-    return data;
-  } else {
-    const { data } = await axios.get("/api/users/currentuser");
-
-    return data;
-  }
-  
-  return {};
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get("/api/users/currentuser");
+  return data;
 };
 
 export default LandingPage;
